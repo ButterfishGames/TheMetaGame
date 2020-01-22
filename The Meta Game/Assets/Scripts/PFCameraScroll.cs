1919ff789137ee64bd0d092891ec37f3b6e68386
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-01-22 11:14:01 -0500

    Began building demo level, added vertical camera follow and camera limits

--- The Meta Game/Assets/Scripts/PFCameraScroll.cs
+++ The Meta Game/Assets/Scripts/PFCameraScroll.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PFCameraScroll : MonoBehaviour
 {
+    [Tooltip("The difference in y position between the camera and the player")]
+    public float yOffset;
+
+    [Tooltip("The lowest x and y coordinates the camera should be able to reach")]
+    public Vector2 min;
+
+    [Tooltip("The highest x and y coordinates the camera should be able to reach")]
+    public Vector2 max;
+
     /// <summary>
     /// Object reference for the Player object
     /// </summary>
@@ -18,9 +27,17 @@ public class PFCameraScroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float posX;
+
         if (player.position.x > transform.position.x)
         {
-            transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            posX = player.position.x;
         }
+        else
+        {
+            posX = transform.position.x;
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(posX, min.x, max.x), Mathf.Clamp(player.position.y, min.y, max.y) + yOffset, transform.position.z);
     }
 }
