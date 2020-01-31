5691a9d0a75a1ef2b6f25da66c9dc83c6c9ad665
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-01-31 11:59:40 -0500

    Fixed Magic Menu scrolling

--- The Meta Game/Assets/Scripts/MonoBehaviours/PosTest.cs
+++ The Meta Game/Assets/Scripts/MonoBehaviours/PosTest.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PosTest : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(GetComponent<RectTransform>().localPosition.y);
+        Debug.Log(GetComponentInChildren<TextMeshProUGUI>().text + ": " + GetComponent<RectTransform>().localPosition.y);
     }
 }
