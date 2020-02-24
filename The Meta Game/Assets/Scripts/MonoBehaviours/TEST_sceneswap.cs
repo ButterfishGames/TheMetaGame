82ecd5aa206b3c2c0685ad2ea608bc793c286d31
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-02-24 13:24:13 -0500

    Added demo level swap script, added triggers to track indoor v outdoor

---
+++ The Meta Game/Assets/Scripts/MonoBehaviours/TEST_sceneswap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_sceneswap : MonoBehaviour
+{
+    public static TEST_sceneswap singleton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (singleton != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            GameController.singleton.StartCoroutine(GameController.singleton.FadeAndLoad(1));
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            GameController.singleton.StartCoroutine(GameController.singleton.FadeAndLoad(3));
+        }
+    }
+}
