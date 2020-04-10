01f2ca8bdbb04fc38f460e55914c6d2f9a794aa4
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-04-10 10:26:59 -0400

    Audio settings, fixed rhythm gameplay setting, added placeholder pickups

--- The Meta Game/Assets/Scripts/MonoBehaviours/Singletons/SettingsController.cs
+++ The Meta Game/Assets/Scripts/MonoBehaviours/Singletons/SettingsController.cs
@@ -7,9 +7,13 @@ public class SettingsController : MonoBehaviour
 {
     public static SettingsController singleton;
 
-    [Header("SettingsPanels")]
+    [Header("Settings Panels")]
     public GameObject gameplayPanel;
     public GameObject displayPanel, audioPanel, controlPanel;
+
+    [Header("Settings Panel Sprites")]
+    public Sprite gameplaySprite;
+    public Sprite displaySprite, audioSprite, controlSprite;
 
     // Gameplay Setting Vars
     [HideInInspector] public bool pfUpJump = true;
@@ -32,6 +36,14 @@ public class SettingsController : MonoBehaviour
     public GameObject pfUpJumpHolder, fgLabel, fgUpJumpHolder, rhythmLabel, dInputHolder, fpsLabel,
         invertXHolder, invertYHolder, sensitivityHolder;
 
+    // Audio Setting Vars
+    [HideInInspector] public float musicVolume = 0.7f;
+    [HideInInspector] public float sfxVolume = 0.8f;
+
+    [Header("Audio Settings UI")]
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+
     private enum Panel
     {
         gameplay,
@@ -42,11 +54,17 @@ public class SettingsController : MonoBehaviour
 
     private Panel currentPanel;
 
+    private Image panelImg;
+
     private void Start()
     {
+        panelImg = GetComponent<Image>();
+
+        // Gameplay Settings setup
         if (PlayerPrefs.HasKey("pfUpJump"))
         {
             pfujToggle.isOn = bool.Parse(PlayerPrefs.GetString("pfUpJump"));
+            pfUpJump = pfujToggle.isOn;
         }
         else
         {
@@ -58,6 +76,7 @@ public class SettingsController : MonoBehaviour
         if (PlayerPrefs.HasKey("fgUpJump"))
         {
             fgujToggle.isOn = bool.Parse(PlayerPrefs.GetString("fgUpJump"));
+            fgUpJump = fgujToggle.isOn;
         }
         else
         {
@@ -69,6 +88,7 @@ public class SettingsController : MonoBehaviour
         if (PlayerPrefs.HasKey("dInput"))
         {
             dInputToggle.isOn = bool.Parse(PlayerPrefs.GetString("dInput"));
+            dInput = dInputToggle.isOn;
         }
         else
         {
@@ -80,6 +100,7 @@ public class SettingsController : MonoBehaviour
         if (PlayerPrefs.HasKey("invertX"))
         {
             invertXToggle.isOn = bool.Parse(PlayerPrefs.GetString("invertX"));
+            invertX = invertXToggle.isOn;
         }
         else
         {
@@ -87,10 +108,11 @@ public class SettingsController : MonoBehaviour
             PlayerPrefs.SetString("invertX", "false");
             PlayerPrefs.Save();
         }
-        
+
         if (PlayerPrefs.HasKey("invertY"))
         {
             invertYToggle.isOn = bool.Parse(PlayerPrefs.GetString("invertY"));
+            invertY = invertYToggle.isOn;
         }
         else
         {
@@ -98,16 +120,55 @@ public class SettingsController : MonoBehaviour
             PlayerPrefs.SetString("invertY", "false");
             PlayerPrefs.Save();
         }
-        
+
         if (PlayerPrefs.HasKey("sensitivity"))
         {
             sensitivitySlider.value = PlayerPrefs.GetFloat("sensitivity");
+            sensitivity = sensitivitySlider.value;
         }
         else
         {
             sensitivitySlider.value = 20;
             PlayerPrefs.SetFloat("sensitivity", 20);
             PlayerPrefs.Save();
+        }
+
+        // Audio Settings setup
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+            musicVolume = musicVolumeSlider.value;
+        }
+        else
+        {
+            musicVolumeSlider.value = 0.7f;
+            PlayerPrefs.SetFloat("musicVolume", 0.7f);
+            PlayerPrefs.Save();
+        }
+
+        if (PlayerPrefs.HasKey("sfxVolume"))
+        {
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+            sfxVolume = sfxVolumeSlider.value;
+        }
+        else
+        {
+            sfxVolumeSlider.value = 0.8f;
+            PlayerPrefs.SetFloat("sfxVolume", 0.8f);
+            PlayerPrefs.Save();
+        }
+
+        AudioSource[] sources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource source in sources)
+        {
+            if (source.gameObject.name.Equals("Player") || source.gameObject.name.Equals("Song"))
+            {
+                source.volume = musicVolume;
+            }
+            else
+            {
+                source.volume = sfxVolume;
+            }
         }
     }
 
@@ -121,11 +182,12 @@ public class SettingsController : MonoBehaviour
         Apply();
 
         //displayPanel.SetActive(false);
-        //audioPanel.SetActive(false);
+        audioPanel.SetActive(false);
         //controlPanel.SetActive(false);
 
         gameplayPanel.SetActive(true);
         currentPanel = Panel.gameplay;
+        panelImg.sprite = gameplaySprite;
 
         if (GameController.singleton.modes[0].unlocked)
         {
@@ -134,6 +196,7 @@ public class SettingsController : MonoBehaviour
             if (PlayerPrefs.HasKey("pfUpJump"))
             {
                 pfujToggle.isOn = bool.Parse(PlayerPrefs.GetString("pfUpJump"));
+                pfUpJump = pfujToggle.isOn;
             }
             else
             {
@@ -155,6 +218,7 @@ public class SettingsController : MonoBehaviour
             if (PlayerPrefs.HasKey("fgUpJump"))
             {
                 fgujToggle.isOn = bool.Parse(PlayerPrefs.GetString("fgUpJump"));
+                fgUpJump = fgujToggle.isOn;
             }
             else
             {
@@ -176,6 +240,7 @@ public class SettingsController : MonoBehaviour
             if (PlayerPrefs.HasKey("dInput"))
             {
                 dInputToggle.isOn = bool.Parse(PlayerPrefs.GetString("dInput"));
+                dInput = dInputToggle.isOn;
             }
             else
             {
@@ -197,6 +262,7 @@ public class SettingsController : MonoBehaviour
             if (PlayerPrefs.HasKey("invertX"))
             {
                 invertXToggle.isOn = bool.Parse(PlayerPrefs.GetString("invertX"));
+                invertX = invertXToggle.isOn;
             }
             else
             {
@@ -209,6 +275,7 @@ public class SettingsController : MonoBehaviour
             if (PlayerPrefs.HasKey("invertY"))
             {
                 invertYToggle.isOn = bool.Parse(PlayerPrefs.GetString("invertY"));
+                invertY = invertYToggle.isOn;
             }
             else
             {
@@ -221,6 +288,7 @@ public class SettingsController : MonoBehaviour
             if (PlayerPrefs.HasKey("sensitivity"))
             {
                 sensitivitySlider.value = PlayerPrefs.GetFloat("sensitivity");
+                sensitivity = sensitivitySlider.value;
             }
             else
             {
@@ -236,6 +304,24 @@ public class SettingsController : MonoBehaviour
             invertYHolder.SetActive(false);
             sensitivityHolder.SetActive(false);
         }
+    }
+
+    public void ShowAudioSettings()
+    {
+        if (audioPanel.activeInHierarchy)
+        {
+            return;
+        }
+
+        Apply();
+
+        gameplayPanel.SetActive(false);
+        //displayPanel.SetActive(false);
+        //controlPanel.SetActive(false);
+
+        audioPanel.SetActive(true);
+        currentPanel = Panel.audio;
+        panelImg.sprite = audioSprite;
     }
 
     public void Apply()
@@ -258,6 +344,29 @@ public class SettingsController : MonoBehaviour
                 PlayerPrefs.SetFloat("sensitivity", sensitivity);
 
                 PlayerPrefs.Save();
+                break;
+
+            case Panel.audio:
+                musicVolume = musicVolumeSlider.value;
+                sfxVolume = sfxVolumeSlider.value;
+
+                PlayerPrefs.SetFloat("musicVolume", musicVolume);
+                PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
+
+                PlayerPrefs.Save();
+
+                AudioSource[] sources = FindObjectsOfType<AudioSource>();
+                foreach (AudioSource source in sources)
+                {
+                    if (source.gameObject.name.Equals("Player") || source.gameObject.name.Equals("Song"))
+                    {
+                        source.volume = musicVolume;
+                    }
+                    else
+                    {
+                        source.volume = sfxVolume;
+                    }
+                }
                 break;
         }
     }
@@ -282,6 +391,29 @@ public class SettingsController : MonoBehaviour
                 PlayerPrefs.SetFloat("sensitivity", sensitivity);
 
                 PlayerPrefs.Save();
+                break;
+
+            case Panel.audio:
+                musicVolumeSlider.value = musicVolume;
+                sfxVolumeSlider.value = sfxVolume;
+
+                PlayerPrefs.SetFloat("musicVolume", musicVolume);
+                PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
+
+                PlayerPrefs.Save();
+
+                AudioSource[] sources = FindObjectsOfType<AudioSource>();
+                foreach (AudioSource source in sources)
+                {
+                    if (source.gameObject.name.Equals("Player") || source.gameObject.name.Equals("Song"))
+                    {
+                        source.volume = musicVolume;
+                    }
+                    else
+                    {
+                        source.volume = sfxVolume;
+                    }
+                }
                 break;
         }
     }
@@ -313,6 +445,32 @@ public class SettingsController : MonoBehaviour
                 PlayerPrefs.SetFloat("sensitivity", 20);
 
                 PlayerPrefs.Save();
+                break;
+
+            case Panel.audio:
+                musicVolume = 0.7f;
+                sfxVolume = 0.8f;
+
+                musicVolumeSlider.value = 0.7f;
+                sfxVolumeSlider.value = 0.8f;
+
+                PlayerPrefs.SetFloat("musicVolume", 0.7f);
+                PlayerPrefs.SetFloat("sfxVolume", 0.8f);
+
+                PlayerPrefs.Save();
+                
+                AudioSource[] sources = FindObjectsOfType<AudioSource>();
+                foreach (AudioSource source in sources)
+                {
+                    if (source.gameObject.name.Equals("Player") || source.gameObject.name.Equals("Song"))
+                    {
+                        source.volume = musicVolume;
+                    }
+                    else
+                    {
+                        source.volume = sfxVolume;
+                    }
+                }
                 break;
         }
     }
