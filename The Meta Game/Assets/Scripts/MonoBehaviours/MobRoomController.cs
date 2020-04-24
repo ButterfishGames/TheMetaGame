dbc423253193ad8ac0c85e228eadf4001d248fbb
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-04-24 15:35:21 -0400

    Mob rooms do mob room things mob roomily

---
+++ The Meta Game/Assets/Scripts/MonoBehaviours/MobRoomController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobRoomController : MonoBehaviour
+{
+    public Spawner[] spawners;
+    public GameObject blockers;
+    public int numEnemies;
+    private int spawnCounter;
+    private int defeatCounter;
+
+    private void Start()
+    {
+        foreach(Spawner spawner in spawners)
+        {
+            spawner.SetMRC(this);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartMobRoom();
+        }
+    }
+
+    private void StartMobRoom()
+    {
+        blockers.SetActive(true);
+        foreach (Spawner spawner in spawners)
+        {
+            spawner.enabled = true;
+        }
+        spawnCounter = 0;
+        defeatCounter = 0;
+    }
+
+    private void StopMobRoom()
+    {
+        blockers.SetActive(false);
+    }
+
+    public void IncSpawnCounter()
+    {
+        spawnCounter++;
+        if (spawnCounter >= numEnemies)
+        {
+            foreach(Spawner spawner in spawners)
+            {
+                spawner.enabled = false;
+            }
+        }
+        Debug.Log(spawnCounter);
+    }
+
+    public void IncDefeatCounter()
+    {
+        defeatCounter++;
+        if (defeatCounter >= numEnemies)
+        {
+            StopMobRoom();
+        }
+        Debug.Log(defeatCounter);
+    }
+}
