efd5a13b19971d86f1395b5f62cd7ad8280d5bb4
daynaclaire <51805898+daynaclaire@users.noreply.github.com>
2020-04-26 17:27:15 -0400

    SFX and MX Sliders are now functional!!!!!

--- The Meta Game/Assets/Scripts/Audio/MixLevels.cs
+++ The Meta Game/Assets/Scripts/Audio/MixLevels.cs
@@ -3,13 +3,10 @@ using System.Collections;
 using UnityEngine.Audio;
 
 public class MixLevels : MonoBehaviour
-{
-    public AudioMixer masterMixer;
+{ 
 
     public void SetSfxLvl(float sfxLvl)
     {
-
-        masterMixer.SetFloat("SFX_Volume", sfxLvl);
         AkSoundEngine.SetRTPCValue("SFX_Volume", sfxLvl);
   
         }
@@ -17,7 +14,6 @@ public class MixLevels : MonoBehaviour
 
     public void SetMxLvl(float mxLvl)
     {
-        masterMixer.SetFloat("MX_Volume", mxLvl);
         AkSoundEngine.SetRTPCValue("MX_Volume", mxLvl);
     }
 
