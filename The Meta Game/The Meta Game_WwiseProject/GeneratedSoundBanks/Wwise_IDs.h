9ed15baa1ffd995b3e10c9c3b89faf4221bc60ca
philipquan <61714379+philipquan@users.noreply.github.com>
2020-04-30 22:51:17 -0400

    WWise
    Finished mixing implemented SFX
    added SFX Busses
    Generated Soundbanks

---
+++ The Meta Game/The Meta Game_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,107 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BOSS_FIGHT = 3688152761U;
+        static const AkUniqueID CASTLE_APPROACH = 2926820552U;
+        static const AkUniqueID CASTLE_ROSE = 491254729U;
+        static const AkUniqueID CRYSTAL_CAVERNS = 2294576446U;
+        static const AkUniqueID DATING_SIM = 3625439640U;
+        static const AkUniqueID DEATH_JINGLE = 3131203835U;
+        static const AkUniqueID DEATH_JINGLE_MUTEMUSIC = 4150336450U;
+        static const AkUniqueID DEV_ROOM = 84803132U;
+        static const AkUniqueID END_CREDITS = 3797583859U;
+        static const AkUniqueID FINAL_BOSS = 2345047989U;
+        static const AkUniqueID MAIN_MENU = 2005704188U;
+        static const AkUniqueID RHYTHM_MODE = 1431648729U;
+        static const AkUniqueID RPG_BATTLE = 332969635U;
+        static const AkUniqueID SFX_COLLECTABLE = 673464333U;
+        static const AkUniqueID SFX_FLAGCHECKPOINT = 2688667465U;
+        static const AkUniqueID SFX_GAME_MODE_SWITCH = 832314226U;
+        static const AkUniqueID SFX_GAME_MODE_UNLOCKED = 2587904569U;
+        static const AkUniqueID SFX_JUMP = 3695098761U;
+        static const AkUniqueID SFX_KICK = 2043286607U;
+        static const AkUniqueID SFX_KNIGHT_TALK = 1803239799U;
+        static const AkUniqueID SFX_KNIGHTCHARGE = 2518317382U;
+        static const AkUniqueID SFX_MAGIC_FIRE = 1965088569U;
+        static const AkUniqueID SFX_PUNCH = 643452507U;
+        static const AkUniqueID SFX_RPG_ATTACK = 4013886705U;
+        static const AkUniqueID SFX_START_GAME = 527172796U;
+        static const AkUniqueID SFX_UI_BACK = 2405914575U;
+        static const AkUniqueID SFX_UPPERCUT = 3797926101U;
+        static const AkUniqueID SFX_WALLJUMP = 3875906189U;
+        static const AkUniqueID START_GAME = 1114964412U;
+        static const AkUniqueID SUNDERED_CASTLE = 2420110896U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMEPLAY_STATEGROUP
+        {
+            static const AkUniqueID GROUP = 4070416336U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BOSS_1 = 1272574122U;
+                static const AkUniqueID CASTLE_APPROACH = 2926820552U;
+                static const AkUniqueID CASTLE_ROSE = 491254729U;
+                static const AkUniqueID CRYSTAL_CAVERNS = 2294576446U;
+                static const AkUniqueID DATING_SIM = 3625439640U;
+                static const AkUniqueID DEV_ROOM = 84803132U;
+                static const AkUniqueID END_CREDITS = 3797583859U;
+                static const AkUniqueID FINAL_BOSS = 2345047989U;
+                static const AkUniqueID MAIN_MENU = 2005704188U;
+                static const AkUniqueID RHYTHM_MODE = 1431648729U;
+                static const AkUniqueID RPG_BATTLE = 332969635U;
+                static const AkUniqueID SUNDERED_CASTLE = 2420110896U;
+            } // namespace STATE
+        } // namespace GAMEPLAY_STATEGROUP
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MX_VOLUME = 1441253587U;
+        static const AkUniqueID SFX_VOLUME = 1564184899U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID DEATH_JINGLE = 3131203835U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID DEXTRA_SOUNDBANK = 3360591469U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID LD_BUS = 879536147U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MX = 1685527054U;
+        static const AkUniqueID NORMAL_BUS = 3243272242U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
