5f48ab45c1037f70349c870176bdc46e28dc37e5
philipquan <61714379+philipquan@users.noreply.github.com>
2020-04-30 23:30:54 -0400

    SFX Implentation
    Implemented Fight SFX
    punch
    uppercut
    kick.
    
    Need to redo uppercut and kick sfx tho since uppercut sounds stronger

--- The Meta Game/Assets/Scripts/MonoBehaviours/Movers/FGController.cs
+++ The Meta Game/Assets/Scripts/MonoBehaviours/Movers/FGController.cs
@@ -255,6 +255,7 @@ public class FGController : Mover
         }
         else
         {
+            AkSoundEngine.PostEvent("sfx_punch", gameObject);
             heldAttackButton[0] = true;
             BasicAttack(Attack.light, lightAttackStats.hitboxActivationTime, lightAttackStats.moveLag, lightAttackStats.xVelocity, lightAttackStats.yVelocity, lightAttackStats.hitstun, lightAttackStats.damage, lightAttackStats.startup, "lightattack");
         }
@@ -297,6 +298,7 @@ public class FGController : Mover
         }
         else
         {
+            AkSoundEngine.PostEvent("sfx_uppercut", gameObject);
             heldAttackButton[1] = true;
             BasicAttack(Attack.medium, mediumAttackStats.hitboxActivationTime, mediumAttackStats.moveLag, mediumAttackStats.xVelocity, mediumAttackStats.yVelocity, mediumAttackStats.hitstun, mediumAttackStats.damage, mediumAttackStats.startup, "mediumattack");
         }
@@ -339,6 +341,7 @@ public class FGController : Mover
         }
         else
         {
+            AkSoundEngine.PostEvent("sfx_kick", gameObject);
             heldAttackButton[2] = true;
             BasicAttack(Attack.heavy, heavyAttackStats.hitboxActivationTime, heavyAttackStats.moveLag, heavyAttackStats.xVelocity, heavyAttackStats.yVelocity, heavyAttackStats.hitstun, heavyAttackStats.damage, heavyAttackStats.startup, "heavyattack");
         }
